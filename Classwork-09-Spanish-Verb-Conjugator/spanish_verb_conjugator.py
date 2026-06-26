1218f769748af43cc192a3d54d4cf3f9263128ec
Ivanna Nunez <2509142@upy.edu.mx>
2026-06-25 18:27:12 -0600

    Add Classwork-09 Spanish Verb Conjugator pseudocode, flowchart, python code  and AI declaration

---
+++ Classwork-09-Spanish-Verb-Conjugator/spanish_verb_conjugator.py
@@ -0,0 +1,19 @@
+pronouns=["Yo", "Tú", "Él", "Nosotros", "Vosotros", "Ellos"]
+
+endings={
+    "ar": ["o", "as", "a", "amos", "ais", "an"],
+    "er": ["o", "es", "e", "emos", "eis", "en"],
+    "ir": ["o", "es", "e", "imos", "is", "en"]
+    }
+
+#INPUT
+verb=input("Write a spanish verb(ar/er/ir): ")
+
+stem=verb[:-2]
+ending=verb[-2:]
+
+conjugations=endings[ending]
+
+for index, pronoun in enumerate(pronouns):
+    termination = conjugations[index]
+    print(f"{pronoun} {stem}{termination}")
